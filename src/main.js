b1aa27ef1e69cc7491db2a49d8a3a802e78c3684
“chenjinwei” <507725948@qq.com>
2025-10-16 08:45:53 +0800

    添加element 样式修改

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
+
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import './assets/css/element_modify.less'
+
 import App from './App.vue';
 
 import router from './router'
