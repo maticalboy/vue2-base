9776efbfa142c67747fb89097cbb7cc8e986a7bd
“chenjinwei” <507725948@qq.com>
2025-10-27 16:55:24 +0800

    添加默认颜色数据

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -153,7 +153,19 @@ export default {
     },
     props: {
         myColor: [String, Number],
-        predefine: Array,
+        predefine:{
+            type:Array,
+            default:[
+                ['#AFC5A1', '#B2D2C0', '#B0DCD5', '#CDE081', '#CCE2A3', '#A5D8B7', '#285E1E'],
+                ['#C2BBDC', '#D2CCE6', '#DAB4D4', '#ECCEE2', '#F9E8F1', '#E6C6E0', '#453B7F'],
+                ['#F4BAAE', '#EBCFB0', '#FBD9BD', '#DECEBD', '#EAC2A6', '#E8B699', '#8D373C'],
+                ['#FDE0A5', '#F9EAD2', '#FFF7C5', '#ECEAAC', '#FCF8BB', '#FBE8A6', '#926A34'],
+                ['#A1CBED', '#B0DCDA', '#BDD7CA', '#BBC1D0', '#C7E8FA', '#A9D5E0', '#335489'],
+                ['#BF7C85', '#ECA37A', '#E3CECD', '#F4B3C2', '#F6DDDB', '#E9A59A', '#A72126'],
+                ['#F3F2F7', '#F1F9FE', '#FFF1D5', '#F6F1DA', '#FFF5D7', '#EAE5DA', '#EDDBB1'],
+                ['#000000', '#2A2A2A', '#555555', '#808080', '#AAAAAA', '#D5D5D5', '#FFFFFF'],
+            ],
+        },
         size: {
             type: String,
         },
